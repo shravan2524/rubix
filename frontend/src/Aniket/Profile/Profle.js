80be0f7b6732796177132751bdc0717d73a10729
Arnold1506 <aniketm1506@gmail.com>
2022-01-16 06:24:30 +0530

    hello world

---
+++ frontend/src/Aniket/Profile/Profle.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Profile.module.css";
+
+const Profile = () => {
+  return (
+    <React.Fragment>
+      <div className={styles["main-container"]}>
+
+      </div>
+    </React.Fragment>
+  );
+};
+
+
+export default Profile;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+
+import Header from "./Aniket/Header/Header";
+import Login from "./Aniket/Login/Login";
+import Profile from "./Aniket/Profile/Profle";
+import Homepage from "./Aniket/Homepage/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/* <Header /> */}
+    <Login />
+    {/* <Profile /> */}
+    {/* <Header />
+    <Homepage /> */}
+    </React.Fragment>
   );
 }
 
